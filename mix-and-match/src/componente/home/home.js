82c5f25ce1530408550c52c6b5648f34f1392089
Rafael Amorim Pereira <Rafael3amorim@gmail.com>
2025-05-13 23:38:45 -0300

    .

--- mix-and-match/src/componente/home/home.js
+++ mix-and-match/src/componente/home/home.js
@@ -5,7 +5,7 @@ export default function Home({ onNavigate }) {
     return (
         <div className="Home">
             <div className="container-home">
-                <h1 className="title-home">Emoções</h1>
+                <h1 className="title-home">Brincando de sentir</h1>
                 <p className="p-home">Este jogo é uma ferramenta terapêutica pensada para crianças. De forma lúdica e segura, ele ajuda a expressar sentimentos, fortalecer o vínculo com o terapeuta e trabalhar emoções difíceis de serem verbalizadas, promovendo autoconhecimento e desenvolvimento emocional.</p>
                 <button onClick={onNavigate} className="button-home">Vamos Brincar!</button>
             </div>
